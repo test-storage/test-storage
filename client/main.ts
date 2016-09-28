510b55544d8d00ca030ed2d0e5b9c5717f0c52d1
Dmitry Zakharov <pumano@gmail.com>
2016-09-28 04:38:08 +0300

    angular2 preparations and dependencies

--- client/main.ts
+++ client/main.ts
@@ -1,1 +1,8 @@
-import {bootstrap} from "angular2/platform/browser";
+// import { enableProdMode } from "@angular/core";
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+// enableProdMode();
+
+platformBrowserDynamic().bootstrapModule(AppModule);
