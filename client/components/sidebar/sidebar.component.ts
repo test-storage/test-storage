af900b291fb744f67120785475c6c8662e1e824d
Dmitry Zakharov <pumano@gmail.com>
2016-11-19 04:49:40 +0300

    #17 Sidebar init

---
+++ client/components/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'ts-sidebar',
+    templateUrl: 'sidebar.component.html'
+})
+export class SidebarComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
