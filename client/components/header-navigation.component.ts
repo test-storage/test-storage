873c18fcaed62bef20458754548cea9489720f35
Dmitry Zakharov <pumano@gmail.com>
2016-11-24 15:00:25 +0300

    logout from header added

--- client/components/header-navigation.component.ts
+++ client/components/header-navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthGuard } from './../guards/auth.guard';
+import { AuthenticationService } from '../services/auth/authentication.service';
 
 @Component({
     moduleId: module.id,
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['header-navigation.component.css']
 })
 export class HeaderNavigationComponent implements OnInit {
-    constructor() { }
+    constructor(
+        private router: Router,
+        private authGuard: AuthGuard,
+        private authenticationService: AuthenticationService) { }
 
     ngOnInit() { }
+
+    logout() {
+        this.authenticationService.logout();
+        this.authGuard.canActivate();
+    }
 }
