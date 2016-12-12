e9e32d5aa83857389e30027eb6a20e505f038af3
Dmitry Zakharov <pumano@gmail.com>
2016-12-12 03:40:23 +0300

    default username changed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -41,9 +41,9 @@ var auth = {
   validate: function(username, password) {
     // spoofing the DB response for simplicity
     var dbUserObj = { // spoofing a userobject from the DB. 
-      name: 'arvind',
+      name: 'admin',
       role: 'admin',
-      username: 'arvind@myapp.com'
+      username: 'admin@test-storage.local'
     };
  
     return dbUserObj;
@@ -52,9 +52,9 @@ var auth = {
   validateUser: function(username) {
     // spoofing the DB response for simplicity
     var dbUserObj = { // spoofing a userobject from the DB. 
-      name: 'arvind',
+      name: 'admin',
       role: 'admin',
-      username: 'arvind@myapp.com'
+      username: 'admin@test-storage.local'
     };
  
     return dbUserObj;
--- test/server/integration/routes/testsuites.js
+++ test/server/integration/routes/testsuites.js
@@ -130,7 +130,7 @@ function loginUser() {
     return function(done) {
         request(server.app)
             .post('/login')
-            .send({ username: 'arvind@myapp.com', password: 'pass123' })
+            .send({ username: 'admin@test-storage.local', password: 'pass123' })
             .end(onResponse);
 
         function onResponse(err, res) {
