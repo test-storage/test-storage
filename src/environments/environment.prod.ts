6927d567b07a4f71a3cfbeca94299dce36757873
Dmitry Zakharov <pumano@gmail.com>
2017-04-16 13:10:28 +0300

    small changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,28 +1,3 @@
 export const environment = {
-  production: true,
-  name: 'production',
-  app: {
-    port: {
-      http: 3000,
-      https: 8443
-    },
-    httpsEnabled: false,
-    privateKey: 'sslcert/server.key',
-    certificate: 'sslcert/server.crt'
-  },
-  db: {
-    name: 'test-storage',
-    path: 'mongodb://localhost',
-    user: 'admin',
-    password: 'admin',
-    port: 27017
-  },
-  errorHandlerOptions: {
-    dumpExceptions: true,
-    showStack: true
-  },
-  logger: {
-    api: 'logs/api.log',
-    exception: 'logs/exceptions.log'
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,30 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  name: 'development',
-  app: {
-    port: {
-      http: 3000,
-      https: 8443
-    },
-    httpsEnabled: false,
-    privateKey: 'sslcert/server.key',
-    certificate: 'sslcert/server.crt'
-  },
-  db: {
-    name: 'test-storage',
-    path: 'mongodb://localhost',
-    user: 'admin',
-    password: 'admin',
-    port: 27017
-  },
-  errorHandlerOptions: {
-    dumpExceptions: true,
-    showStack: true
-  },
-  logger: {
-    api: 'logs/api.log',
-    exception: 'logs/exceptions.log'
-  }
+  production: false
 };
