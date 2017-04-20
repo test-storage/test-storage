2f588ecc9caa6d55259fdf76dd094cfa8dd9149d
Dmitry Zakharov <pumano@gmail.com>
2017-04-20 13:09:35 +0300

    small fix

--- src/app/services/auth/authentication.service.ts
+++ src/app/services/auth/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { contentHeaders } from './headers';
 
