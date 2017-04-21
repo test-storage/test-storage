f2458618c2f3dd6b199e4f333358fada18aca8c5
Dmitry Zakharov <pumano@gmail.com>
2017-04-21 14:31:00 +0300

    travis ci chrome fix

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,15 @@
 
 module.exports = function (config) {
   config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', '@angular/cli'],
+
+
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -15,9 +22,14 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
+    // list of files / patterns to load in the browser
     files: [
       { pattern: './src/test.ts', watched: false }
     ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       './src/test.ts': ['@angular/cli']
     },
@@ -31,16 +43,34 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+
     reporters: config.angularCli && config.angularCli.codeCoverage
       ? ['progress', 'coverage-istanbul']
       : ['progress', 'kjhtml'],
+
+
+    // web server port
     port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-    browsers: ['Chrome_Beta_Headless'],
+
+
+    browsers: ['ChromeCanary'],
     customLaunchers: {
-      Chrome_Beta_Headless: {
+      ChromeCanary: {
         base: 'Chrome',
         flags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222']
       }
@@ -49,6 +79,10 @@ module.exports = function (config) {
       level: 'log',
       terminal: true
     },
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   });
 };
