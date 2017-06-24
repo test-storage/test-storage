ca817869f7638b4829f6784dc3c6229ebaa5ca45
Dmitry Zakharov <pumano@gmail.com>
2017-06-24 13:54:36 +0300

    small ui sidebar polish + packages bumping + drop jquery + drop bootstrap.js e2e test renaming

--- test/client/e2e/tests/LoginTest.e2e-spec.ts
+++ test/client/e2e/tests/LoginTest.e2e-spec.ts
@@ -3,22 +3,22 @@ import { LoginPage } from '../pages/LoginPage';
 import { HomePage } from '../pages/HomePage';
 import { BaseTest } from './BaseTest';
 import { expect } from 'chai';
-import { should } from 'chai-as-promised';
+
 
 describe('Test Storage loginpage', async function() {
-    this.timeout(100000);
+
   it('should successfully redirect to homepage after login action', function(done) {
-    let loginPage = new LoginPage();
-    let homePage = new HomePage();
+    const loginPage = new LoginPage();
+    const homePage = new HomePage();
 
     browser.get('http://localhost:3000', 30000);
     browser.waitForAngular();
-    loginPage.typeLogin('admin@test-storage.local');
-    loginPage.typePassword('pass123');
+    loginPage.typeLogin('admin');
+    loginPage.typePassword('admin');
     loginPage.submitLoginButton();
-        
-    let actual = homePage.isUserLogged();
-    let expected = 'John Doe';
+
+    const actual = homePage.isUserLogged();
+    const expected = 'John Doe';
 
 // expect(actual).to.equal(expected);
 
@@ -26,7 +26,7 @@ describe('Test Storage loginpage', async function() {
         expect(result).to.equal(expected);
         done();
     });
-    
+
   });
-  
+
 });
