d9407396921ea19db602d2fc39b08844ce58ed50
Dmitry Zakharov <pumano@gmail.com>
2017-07-09 18:29:30 +0300

    feat(Notifications): introduce notifications list UI

--- src/app/modules/layout/notifications/notifications.component.ts
+++ src/app/modules/layout/notifications/notifications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './../../../services/theme/theme.service';
 
 @Component({
   selector: 'app-notifications',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public themeService: ThemeService) { }
 
   ngOnInit() {
   }
