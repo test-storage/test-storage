633be983a5a43428a75f9ac032fdd8f10ff4d197
Dmitry Zakharov <pumano@gmail.com>
2017-07-12 16:18:15 +0300

    test(Users): fix test dependencies

--- src/app/modules/users/users.component.spec.ts
+++ src/app/modules/users/users.component.spec.ts
@@ -1,6 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BaseRequestOptions, Http, HttpModule, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { AuthenticationService } from '../../services/auth/authentication.service';
 
 import { UsersComponent } from './users.component';
+import { UserService } from '../../services/user/user.service';
+import { ThemeService } from '../../services/theme/theme.service';
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
@@ -8,9 +15,26 @@ describe('UsersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      declarations: [UsersComponent],
+      providers: [
+        ThemeService,
+        AuthenticationService,
+        UserService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ],
+      imports: [
+        HttpModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +43,7 @@ describe('UsersComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it('should be created', inject([UserService], (service: UserService) => {
     expect(component).toBeTruthy();
-  });
+  }));
 });
