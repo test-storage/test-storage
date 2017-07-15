e7521ff8518fcda908362f7f82f51489ef0c7aa6
Dmitry Zakharov <pumano@gmail.com>
2017-07-16 01:37:13 +0300

    test(Projects): fix tests

--- src/app/modules/projects/projects.component.spec.ts
+++ src/app/modules/projects/projects.component.spec.ts
@@ -3,10 +3,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BaseRequestOptions, Http, HttpModule, Response, ResponseOptions } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
+
+import { ThemeService } from '../../services/theme/theme.service';
+import { NotificationsService } from 'angular2-notifications';
+
 import { AuthenticationService } from '../../services/auth/authentication.service';
 import { ProjectsComponent } from './projects.component';
 import { ProjectService } from '../../services/project/project.service';
-import { ThemeService } from '../../services/theme/theme.service';
+
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
@@ -17,6 +21,7 @@ describe('ProjectsComponent', () => {
       declarations: [ProjectsComponent],
       providers: [
         ThemeService,
+        NotificationsService,
         AuthenticationService,
         ProjectService,
         MockBackend,
