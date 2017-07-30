e1174896f582acece3aac7fb88c67bf7d28ba1d9
Dmitry Zakharov <pumano@gmail.com>
2017-07-30 23:15:12 +0300

    test(db): add db-helper for mock / real db connection and model tests

---
+++ test/server/db-helper.ts
@@ -0,0 +1,38 @@
+import * as config from 'config';
+import * as mongoose from 'mongoose';
+import { Mockgoose } from 'mockgoose';
+
+let mockgoose: Mockgoose = new Mockgoose(mongoose);
+mongoose.Promise = global.Promise;
+
+/**
+ * Database connection helper.
+ *
+ */
+
+async function connectToDB() {
+
+  const connectionString = config.get('db.scheme') +
+    config.get('db.user') + ':' + config.get('db.password') + '@' +
+    config.get('db.host') + '/' + config.get('db.name');
+
+  const connectionOptions = {
+    useMongoClient: true
+  };
+
+  await mongoose.connect(connectionString, connectionOptions)
+    .then(() => console.log('MongoDB connection successful'))
+    .catch((err) => console.error(err));
+
+}
+
+async function connectToMockDB() {
+  mockgoose.prepareStorage().then(() => {
+    mongoose.connect('mongodb://foobar/baz');
+    mongoose.connection.on('connected', () => {
+      console.log('db connection is now open');
+    });
+  });
+}
+
+export { connectToDB, connectToMockDB };
