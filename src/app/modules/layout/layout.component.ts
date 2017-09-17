d87656dfb5480ee04c4d906af863b0cfd8f0296b
Dmitry Zakharov <pumano@gmail.com>
2017-09-17 16:44:38 +0300

    chore: remove unused dependencies

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
 import { Router } from '@angular/router';
-import { AuthHttp } from 'angular2-jwt';
 import { AuthenticationService } from '../../services/auth/authentication.service';
 import { ThemeService } from './../../services/theme/theme.service';
 
