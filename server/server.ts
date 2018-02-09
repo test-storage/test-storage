b1e91a131473fd63f58dd096e21ef0f48386d2a9
Dmitry Zakharov <pumano@gmail.com>
2018-02-09 17:13:56 +0300

    refactor(server): implement httpsOptions support

--- server/server.ts
+++ server/server.ts
@@ -7,42 +7,47 @@ import * as morgan from 'morgan';
 import * as compression from 'compression';
 
 import { NestFactory } from '@nestjs/core';
+import { HttpsOptions } from '@nestjs/common/interfaces/https-options.interface';
 import { ApplicationModule } from './modules/app.module';
 import { NotFoundExceptionFilter } from './modules/common/filters/not-found-exception.filter';
 
 import * as config from 'config';
 
 const expressServer = express();
-let server;
+let app;
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(ApplicationModule, expressServer);
-  app.useGlobalFilters(new NotFoundExceptionFilter());
-  if (config.get('app.enableGzipCompression') === true) {
-    app.use(compression());
-  }
-  app.use(morgan('dev'));
-  // Point static path to dist
-  app.use(express.static(path.join(__dirname, '../../dist')));
-  app.use('/i18n', express.static(path.join('./i18n')));
-  await app.init();
-
-
   if (config.get('app.httpsEnabled') === false) {
-
-    server = await http.createServer(expressServer).listen(3000);
-
+    app = await NestFactory.create(ApplicationModule, expressServer);
   } else {
-
-    const httpsOptions = {
+    const httpsOptions: HttpsOptions = {
       key: fs.readFileSync(config.get('https.privateKey'), 'utf8'),
       cert: fs.readFileSync(config.get('https.certificate'), 'utf8')
     };
-
-    server = await https.createServer(httpsOptions, expressServer).listen(443);
-
+    app = await NestFactory.create(ApplicationModule, expressServer, httpsOptions);
   }
+
+  setMiddlewares();
+
+  await app.init();
+
 }
+
+async function setMiddlewares() {
+
+  app.useGlobalFilters(new NotFoundExceptionFilter());
+
+  if (config.get('app.enableGzipCompression') === true) {
+    app.use(compression());
+  }
+
+  app.use(morgan('dev'));
+
+  // Point static path to dist
+  app.use(express.static(path.join(__dirname, '../../dist')));
+  app.use('/i18n', express.static(path.join('./i18n')));
+
+}
+
 bootstrap();
-export { server };
