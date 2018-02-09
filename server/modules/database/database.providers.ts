3685565a26126e3a849a26442ba54e5ff517dd1b
Dmitry Zakharov <pumano@gmail.com>
2018-02-09 13:03:42 +0300

    chore: remove explicit mongo connection options according to mongoose 5

--- server/modules/database/database.providers.ts
+++ server/modules/database/database.providers.ts
@@ -8,9 +8,7 @@ export const databaseProviders = [
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<mongoose.Connection> => {
       (mongoose as any).Promise = global.Promise;
-      return await mongoose.connect(process.env.MONGOLAB_URI || connectionString, {
-        useMongoClient: true,
-      });
+      return await mongoose.connect(process.env.MONGOLAB_URI || connectionString);
     },
   },
 ];
