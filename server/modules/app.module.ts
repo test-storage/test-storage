935c2749b293e481e34ee45f86c823fdc40d773d
Dmitry Zakharov <pumano@gmail.com>
2018-03-20 23:34:56 +0300

    chore: cleanup

--- server/modules/app.module.ts
+++ server/modules/app.module.ts
@@ -12,9 +12,8 @@ import { RolesModule } from './roles/roles.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TestsuitesModule } from './testsuites/testsuites.module';
 
-console.log(process.env.DOCKERIZED);
-const connectionString = `mongodb://${config.get('db.user')}:${config.get('db.password')}@${process.env.DOCKERIZED ? 'mongodb' : config.get('db.host')}/${config.get('db.name')}?authSource=admin`;
-console.log(connectionString);
+const connectionString = `mongodb://${config.get('db.user')}:${config.get('db.password')}@${process.env.DOCKERIZED ? 'mongodb' : config.get('db.host')}/${config.get('db.name')}`;
+
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGOLAB_URI || connectionString),
