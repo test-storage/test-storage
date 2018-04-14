9c6b1b0a8f6938d3ef6d46c417004abc9b1cbcfe
Dmitry Zakharov <pumano@gmail.com>
2018-04-15 00:49:14 +0300

    feat(executions): add test execution details

---
+++ src/app/test-executions/test-executions-details/test-executions-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestExecutionsDetailsComponent } from './test-executions-details.component';
+
+describe('TestExecutionsDetailsComponent', () => {
+  let component: TestExecutionsDetailsComponent;
+  let fixture: ComponentFixture<TestExecutionsDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestExecutionsDetailsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestExecutionsDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
