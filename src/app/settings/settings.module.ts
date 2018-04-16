c807fe4f9f07b9c071eb6ff8e8cc46be4646bd0d
Dmitry Zakharov <pumano@gmail.com>
2018-04-16 23:54:58 +0300

    feat(settings): add user management component

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
+import { SharedModule } from '../shared/shared.module';
+import { UsersComponent } from '../users/users.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    SharedModule
   ],
-  declarations: [SettingsComponent]
+  declarations: [
+    SettingsComponent,
+    UsersComponent
+  ]
 })
 export class SettingsModule { }
