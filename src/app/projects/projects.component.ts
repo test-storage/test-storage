cef51135739d5ca3da25ed3cd6e1140e5c8a002f
Dmitry Zakharov <pumano@gmail.com>
2018-04-20 14:09:49 +0300

    feat(projects): empty state UI for projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { pageTransition } from '../animations';
 
+import { Project } from './project';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -13,9 +15,18 @@ export class ProjectsComponent implements OnInit {
   @HostBinding('style.display') display = 'block';
   @HostBinding('style.position') position = 'absolute';
 
+
+  projects: Project[] = [];
+  projectWizardOpened = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  create() {
+    this.projectWizardOpened = true;
+    // TODO create project wizard
   }
 
 }
