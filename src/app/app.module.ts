ca7d64cdf9d4ac169b536a323879c395d04cb16d
Dmitry Zakharov <pumano@gmail.com>
2018-04-21 01:09:17 +0300

    feat(routes): dashboard route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 import { SharedModule } from './shared/shared.module';
 
-
 @NgModule({
   declarations: [
     AppComponent,
