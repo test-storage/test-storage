a65329f3fc02b1d4f7d9ed0ac8052abfcb55ed39
Dmitry Zakharov <pumano@gmail.com>
2018-04-21 01:09:17 +0300

    feat(routes): dashboard route

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
