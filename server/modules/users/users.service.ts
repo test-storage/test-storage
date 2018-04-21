3cff02da69bf61723efa36e488fdc77b92e7b4c4
Dmitry Zakharov <pumano@gmail.com>
2018-04-21 15:48:45 +0300

    feat(server): users/me route and remove password from response

--- server/modules/users/users.service.ts
+++ server/modules/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Model } from 'mongoose';
-import { Component } from '@nestjs/common';
+import { Component, Inject } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { UserSchema } from './user.schema';
@@ -9,7 +9,7 @@ import { User } from './user.interface';
 @Component()
 export class UsersService {
 
-  constructor( @InjectModel(UserSchema) private readonly userModel: Model<User>) { }
+  constructor(@InjectModel(UserSchema) private readonly userModel: Model<User>) { }
 
   async create(userDto: CreateUserDto): Promise<User> {
     const createdUser = new this.userModel(userDto);
@@ -19,22 +19,29 @@ export class UsersService {
   }
 
   async findAll(): Promise<User[]> {
-    return await this.userModel.find().exec();
+    return await this.userModel.find().select('-password').exec();
   }
 
   async findOne(id: string): Promise<User> {
-    return await this.userModel.findOne({ '_id': id }).exec();
+    return await this.userModel.findOne({ '_id': id }).select('-password').exec();
   }
 
   async findOneByUsername(username: string): Promise<User> {
+    // only use for AUTH purposes
     return await this.userModel.findOne({ 'email': username }).exec();
   }
 
   async update(id: string, user: CreateUserDto): Promise<User> {
-    return await this.userModel.findOneAndUpdate({ '_id': id }, user).exec();
+    // TODO check update password
+    return await this.userModel.findOneAndUpdate({ '_id': id }, user).select('-password').exec();
   }
 
   async delete(id: string): Promise<void> {
-    return await this.userModel.findOneAndRemove({ '_id': id }).exec();
+    return await this.userModel.findOneAndRemove({ '_id': id }).select('-password').exec();
+  }
+
+  async findMe(email: string): Promise<User> {
+    // TODO get user id from token
+    return await this.userModel.findOne({ 'email': email }).select('-password').exec();
   }
 }
