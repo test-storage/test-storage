4e15fc0de5a259123a67edc9414ebbf8fb7df927
Dmitry Zakharov <pumano@gmail.com>
2018-04-23 17:32:46 +0300

    docs(server): add swagger metadata for test cases and test suites

--- server/modules/testcases/testcases.controller.ts
+++ server/modules/testcases/testcases.controller.ts
@@ -23,26 +23,45 @@ export class TestcasesController {
   constructor(private readonly testcasesService: TestcasesService) { }
 
   @Post()
+  @ApiOperation({ title: 'Create Test Case' })
+  @ApiResponse({ status: 201, description: 'The test case has been successfully created.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async create( @Body(new ValidationPipe()) createTestcaseDto: CreateTestcaseDto) {
     this.testcasesService.create(createTestcaseDto);
   }
 
   @Get()
+  @ApiOperation({ title: 'Get All Test Suites by Test Suite id' })
+  @ApiResponse({ status: 200, description: 'The list of test cates by test suite id has been successfully retrieved.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findByTestSuiteId(@Query('testSuiteId', new ParameterValidationPipe()) id: string): Promise<Testcase[]> {
     return this.testcasesService.findAllByTestSuiteId(id);
   }
 
   @Get()
+  @ApiOperation({ title: 'Get All Test Cases' })
+  @ApiResponse({ status: 200, description: 'The list of test cases has been successfully retrieved.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findAll(): Promise<Testcase[]> {
     return this.testcasesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ title: 'Get Single Test Case by id' })
+  @ApiResponse({ status: 200, description: 'The single test case has been successfully retrieved.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findOne( @Param('id', new ParameterValidationPipe()) id: string): Promise<Testcase> {
     return this.testcasesService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({ title: 'Update Single Test Case by id' })
+  @ApiResponse({ status: 200, description: 'The single test case has been successfully updated.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findOneAndUpdate(
     @Body(new ValidationPipe()) createTestcaseDto: CreateTestcaseDto,
     @Param('id', new ParameterValidationPipe()) id: string) {
@@ -50,6 +69,10 @@ export class TestcasesController {
   }
 
   @Delete(':id')
+  @ApiOperation({ title: 'Delete Single Test Case by id' })
+  @ApiResponse({ status: 200, description: 'The single test case has been successfully deleted.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async delete( @Param('id', new ParameterValidationPipe()) id: string) {
     return this.testcasesService.delete(id);
   }
