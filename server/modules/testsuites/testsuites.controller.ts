510a14d24e5e82d463628703617387f8e31e2471
Dmitry Zakharov <pumano@gmail.com>
2018-04-23 17:32:46 +0300

    docs(server): add swagger metadata for test cases and test suites

--- server/modules/testsuites/testsuites.controller.ts
+++ server/modules/testsuites/testsuites.controller.ts
@@ -23,21 +23,36 @@ export class TestsuitesController {
   constructor(private readonly testsuitesService: TestsuitesService) { }
 
   @Post()
+  @ApiOperation({ title: 'Create Test Suite' })
+  @ApiResponse({ status: 201, description: 'The test suite has been successfully created.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async create( @Body(new ValidationPipe()) createTestsuiteDto: CreateTestsuiteDto) {
     this.testsuitesService.create(createTestsuiteDto);
   }
 
   @Get()
+  @ApiOperation({ title: 'Get All Test Suites' })
+  @ApiResponse({ status: 200, description: 'The list of test suites has been successfully retrieved.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findAll(): Promise<Testsuite[]> {
     return this.testsuitesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ title: 'Get Single Test Suite by id' })
+  @ApiResponse({ status: 200, description: 'The single test suite has been successfully retrieved.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findOne( @Param('id', new ParameterValidationPipe()) id: string): Promise<Testsuite> {
     return this.testsuitesService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({ title: 'Update Single Test Suite by id' })
+  @ApiResponse({ status: 200, description: 'The single test suite has been successfully updated.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async findOneAndUpdate(
     @Body(new ValidationPipe()) createTestsuiteDto: CreateTestsuiteDto,
     @Param('id', new ParameterValidationPipe()) id: string) {
@@ -45,6 +60,10 @@ export class TestsuitesController {
   }
 
   @Delete(':id')
+  @ApiOperation({ title: 'Delete Single Test Suite by id' })
+  @ApiResponse({ status: 200, description: 'The single test suite has been successfully deleted.' })
+  @ApiResponse({ status: 400, description: 'Validation failed' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   async delete( @Param('id', new ParameterValidationPipe()) id: string) {
     return this.testsuitesService.delete(id);
   }
