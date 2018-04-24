8d0d79299ac57b9260eb1412f5d45cf0c1cf456c
Dmitry Zakharov <pumano@gmail.com>
2018-04-24 23:51:16 +0300

    fix(i18n): ru locale loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   constructor(translate: TranslateService) {
     translate.addLangs(['en', 'ru']); // , 'de', 'fr', 'es', 'it'
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
+    translate.setDefaultLang('ru');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     // translate.use('en');
