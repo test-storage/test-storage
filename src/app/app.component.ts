40c235d82288dcc59fc9f03a68bb7ef08647e995
Dmitry Zakharov <pumano@gmail.com>
2018-04-24 17:24:48 +0300

    feat(i18n): choose language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,12 @@ export class AppComponent {
   constructor(translate: TranslateService) {
     translate.addLangs(['en', 'ru', 'de', 'fr', 'es', 'it']);
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('ru');
+    translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     // translate.use('en');
-    // const browserLang = translate.getBrowserLang();
-    // translate.use(browserLang.match(/en|ru|de|es|it|fr/) ? browserLang : 'en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|ru|de|es|it|fr/) ? browserLang : 'en');
   }
 
 }
