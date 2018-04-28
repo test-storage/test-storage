65a0e15a743825a8836b17c2431313a60fa6be79
Dmitry Zakharov <pumano@gmail.com>
2018-04-29 02:06:00 +0300

    feat(users): create user modal

---
+++ src/app/settings/users/edit-user-modal/edit-user-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-user-modal',
+  templateUrl: './edit-user-modal.component.html',
+  styleUrls: ['./edit-user-modal.component.css']
+})
+export class EditUserModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
