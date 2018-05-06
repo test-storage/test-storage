a2503d8ececce464f1ef5a6873bb6ec45f2e962c
Dmitry Zakharov <pumano@gmail.com>
2018-05-06 23:57:36 +0300

    feat(testexecutions): add test execution details animation

--- src/app/test-executions/test-executions-details/test-executions-details.component.ts
+++ src/app/test-executions/test-executions-details/test-executions-details.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { pageTransition } from '../../animations';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-test-executions-details',
   templateUrl: './test-executions-details.component.html',
-  styleUrls: ['./test-executions-details.component.css']
+  styleUrls: ['./test-executions-details.component.css'],
+  animations: [pageTransition]
 })
 export class TestExecutionsDetailsComponent implements OnInit {
+
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display') display = 'block';
 
   constructor() { }
 
