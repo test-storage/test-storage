adb39a070f4dbd25bacff72a8dbe4e55dfc01578
Dmitry Zakharov <pumano@gmail.com>
2018-05-25 19:10:12 +0300

    test(autofocus): add tests for autofocus directive

--- src/app/shared/autofocus.directive.spec.ts
+++ src/app/shared/autofocus.directive.spec.ts
@@ -1,8 +1,59 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ElementRef, Component, DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { AutofocusDirective } from './autofocus.directive';
 
+export class MockElementRef {
+  nativeElement = {};
+}
+
+@Component({
+  template: `<input type="text" [appAutofocus]=false>`
+})
+export class TestAutoFocusComponent {}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutofocusDirective();
-    expect(directive).toBeTruthy();
+
+  let component: TestAutoFocusComponent;
+  let fixture: ComponentFixture<TestAutoFocusComponent>;
+  let inputEl: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestAutoFocusComponent, AutofocusDirective ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestAutoFocusComponent);
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.css('input'));
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    // inputEl.nativeElement.appAutofocus = false;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set appAutofocus value to true', async () => {
+
+    inputEl.nativeElement.appAutofocus = true;
+    fixture.detectChanges();
+    expect(inputEl.nativeElement.appAutofocus).toBe(true);
+  });
+
+  xit('should check for focus() event to have been called', () => {
+    spyOn(inputEl.nativeElement, 'focus');
+
+    inputEl.nativeElement.appAutofocus = true;
+    fixture.detectChanges();
+
+    expect(inputEl.nativeElement.focus).toHaveBeenCalled();
   });
 });
