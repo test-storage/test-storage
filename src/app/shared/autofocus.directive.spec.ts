588c2ed2fbded12621c53dc823b2138e483108f0
Dmitry Zakharov <pumano@gmail.com>
2018-05-25 23:25:58 +0300

    test(directives): add more autofocus directive tests

--- src/app/shared/autofocus.directive.spec.ts
+++ src/app/shared/autofocus.directive.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { ElementRef, Component, DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { AutofocusDirective } from './autofocus.directive';
@@ -8,52 +8,60 @@ export class MockElementRef {
 }
 
 @Component({
-  template: `<input type="text" [appAutofocus]=false>`
+  template: `
+  <input type="text" [appAutofocus]=false>
+  <input type="text" [appAutofocus]=true (focus)="onFocus()">`
 })
-export class TestAutoFocusComponent {}
+export class TestAutoFocusComponent {
+  onFocus($event) {
+    console.log($event);
+  }
+}
 
 describe('AutofocusDirective', () => {
 
   let component: TestAutoFocusComponent;
   let fixture: ComponentFixture<TestAutoFocusComponent>;
-  let inputEl: DebugElement;
+  let directives: DebugElement[];
+  let focused: DebugElement[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestAutoFocusComponent, AutofocusDirective ],
-      schemas: [ NO_ERRORS_SCHEMA ]
+      declarations: [TestAutoFocusComponent, AutofocusDirective],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TestAutoFocusComponent);
     component = fixture.componentInstance;
-    inputEl = fixture.debugElement.query(By.css('input'));
-    fixture.detectChanges();
-  });
 
-  afterEach(() => {
-    // inputEl.nativeElement.appAutofocus = false;
+    fixture.detectChanges();
+
+    directives = fixture.debugElement.queryAll(By.directive(AutofocusDirective));
+    focused = fixture.debugElement.queryAll(By.css(':focus'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should set appAutofocus value to true', async () => {
-
-    inputEl.nativeElement.appAutofocus = true;
-    fixture.detectChanges();
-    expect(inputEl.nativeElement.appAutofocus).toBe(true);
+  it('should have two autofocus elements', () => {
+    expect(directives.length).toBe(2);
   });
 
-  xit('should check for focus() event to have been called', () => {
-    spyOn(inputEl.nativeElement, 'focus');
+  it('should set appAutofocus value to false', () => {
+    const actual = directives[0].injector.get(AutofocusDirective) as AutofocusDirective;
+    expect(actual.appAutofocus).toBe(false);
+  });
 
-    inputEl.nativeElement.appAutofocus = true;
-    fixture.detectChanges();
+  it('should set appAutofocus value to true', async () => {
+    const actual = directives[1].injector.get(AutofocusDirective) as AutofocusDirective;
+    expect(actual.appAutofocus).toBe(true);
+  });
 
-    expect(inputEl.nativeElement.focus).toHaveBeenCalled();
+  xit('should check element with :focus exist', async () => {
+    expect(focused.length).toBe(1);
   });
 });
