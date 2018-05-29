ea22f690c008d26b4b6ec64d322c5e9336fa7fad
Dmitry Zakharov <pumano@gmail.com>
2018-05-29 18:16:34 +0300

    feat(testruns): add validation for testruns

--- server/modules/testruns/create-testrun.dto.ts
+++ server/modules/testruns/create-testrun.dto.ts
@@ -1,18 +1,85 @@
+import { ApiModelProperty, ApiModelPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsInt, IsOptional, IsNotEmpty, IsDate, IsBoolean, IsArray } from 'class-validator';
+
 export class CreateTestrunDto {
-  _id?: string;
-  name: string;
-  description?: string;
-  projectId: string;
-  builds?: Array<string>;
-  environments?: Array<string>;
-  platforms?: Array<string>;
-  testcases?: Array<string>;
-  status?: string;
-  archieved?: boolean;
-  startDate?: string;
-  endDate?: string;
-  created?: string;
-  updated?: string;
-  createdBy?: string;
-  updatedBy?: string;
+
+  @ApiModelPropertyOptional({ type: String })
+  @IsOptional()
+  @IsString()
+  readonly _id?: string;
+
+  @ApiModelProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @ApiModelPropertyOptional({ type: String })
+  @IsOptional()
+  @IsString()
+  readonly description?: string;
+
+  @ApiModelProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
+  readonly projectId: string;
+
+  @ApiModelPropertyOptional({ type: Array })
+  @IsOptional()
+  @IsArray()
+  readonly builds?: Array<string>;
+
+  @ApiModelPropertyOptional({ type: Array })
+  @IsOptional()
+  @IsArray()
+  readonly environments?: Array<string>;
+
+  @ApiModelPropertyOptional({ type: Array })
+  @IsOptional()
+  @IsArray()
+  readonly platforms?: Array<string>;
+
+  @ApiModelPropertyOptional({ type: Array })
+  @IsOptional()
+  @IsArray()
+  readonly testcases?: Array<string>;
+
+  @ApiModelPropertyOptional({ type: String })
+  @IsOptional()
+  @IsString()
+  readonly status?: string;
+
+  @ApiModelPropertyOptional({ type: Boolean })
+  @IsBoolean()
+  @IsOptional()
+  readonly archieved?: boolean;
+
+  @ApiModelPropertyOptional({ type: Date })
+  @IsDate()
+  @IsOptional()
+  readonly startDate?: Date;
+
+  @ApiModelPropertyOptional({ type: Date })
+  @IsDate()
+  @IsOptional()
+  readonly endDate?: Date;
+
+  @ApiModelPropertyOptional({ type: Date })
+  @IsDate()
+  @IsOptional()
+  readonly created?: Date;
+
+  @ApiModelPropertyOptional({ type: Date })
+  @IsDate()
+  @IsOptional()
+  readonly updated?: Date;
+
+  @ApiModelPropertyOptional({ type: String })
+  @IsString()
+  @IsOptional()
+  readonly createdBy?: string;
+
+  @ApiModelPropertyOptional({ type: String })
+  @IsString()
+  @IsOptional()
+  readonly updatedBy?: string;
 }
