7fdde3a6a7a10f4a42de886b273c3c1afdac850e
Dmitry Zakharov <pumano@gmail.com>
2018-08-15 19:14:18 +0300

    feat(auth): introduce jwt secret auto generation on first application start

---
+++ server/modules/jwt-init.ts
@@ -0,0 +1,25 @@
+import * as crypto from 'crypto';
+import * as path from 'path';
+import { writeFileSync } from 'fs';
+import { jwtSecret } from './auth/passport/jwt.secret';
+
+export function checkJWTSecret() {
+  if (jwtSecret() === '') {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function generateJWTSecret() {
+  const fileName = path.resolve(__dirname, './auth/passport/jwt.secret.ts');
+
+  const key = crypto.randomBytes(256).toString('hex');
+
+  const data = `export function jwtSecret() {
+    return '${key}';
+}
+`;
+
+  writeFileSync(fileName, data);
+}
